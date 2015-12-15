971d11ffe844d0fc72fa68c7147edeb54d7be024
Sebastian Clarke <sebastian.clarke@envsys.co.uk>
2015-12-15 11:37:12 +0000

    Test proof-of-concept integration of simple function into cobweb-qa library

--- WPS-Process-Development-COBWEB/src/pillar/automaticvalidation/ValueRange.java
+++ WPS-Process-Development-COBWEB/src/pillar/automaticvalidation/ValueRange.java
@@ -1,34 +1,20 @@
 package pillar.automaticvalidation;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import javax.xml.namespace.QName;
-
 import org.apache.log4j.Logger;
-import org.apache.xmlbeans.XmlOptions;
 import org.geotools.data.collection.ListFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
-import org.geoviqua.gmd19157.DQDataQualityType;
-import org.geoviqua.qualityInformationModel.x40.GVQDataQualityType;
-import org.geoviqua.qualityInformationModel.x40.GVQDiscoveredIssueType;
-import org.geoviqua.qualityInformationModel.x40.GVQMetadataDocument;
-import org.geoviqua.qualityInformationModel.x40.GVQMetadataType;
-import org.n52.wps.io.data.GenericFileData;
 import org.n52.wps.io.data.IData;
 import org.n52.wps.io.data.binding.complex.GTVectorDataBinding;
-import org.n52.wps.io.data.binding.complex.GenericFileDataBinding;
 import org.n52.wps.io.data.binding.literal.LiteralDoubleBinding;
 import org.n52.wps.io.data.binding.literal.LiteralStringBinding;
 import org.n52.wps.server.AbstractAlgorithm;
@@ -41,9 +27,11 @@ import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 import com.vividsolutions.jts.geom.Geometry;
 
-public class ValueRange extends AbstractAlgorithm{
+import eu.cobwebproject.qa.automaticvalidation.ValRange;
+
+public class ValueRange extends AbstractAlgorithm {
 	/**
-	 * @author Sam Meek
+	 * @author Sam Meek (Modified by Sebastian Clarke - Environment Systems)
 	 * Process to assess the range of an attribute given a minimum and maximum threshold
 	 * Output is the metadata field "DQ_ValueRange" which is 1 for pass criteria and 0 for not passing
 	 * result is observations with 1 or 0
@@ -51,7 +39,6 @@ public class ValueRange extends AbstractAlgorithm{
 	 */
 	
 	Logger LOG = Logger.getLogger(ValueRange.class);
-
 
 	@Override
 	public Class<?> getInputDataType(String identifier) {
@@ -96,132 +83,128 @@ public class ValueRange extends AbstractAlgorithm{
 	public Map<String, IData> run(Map<String, List<IData>> inputData)
 			throws ExceptionReport {
 
+		// Get attributes from WPS Call (as lists for some reason)
 		List<IData> obsList = inputData.get("inputObservations");
 		List<IData> maxList = inputData.get("maxRange");
 		List<IData> minList = inputData.get("minRange");
+		List<IData> attributeNameList = inputData.get("attributeName");
 		
-		List <IData> attributeNameList = inputData.get("attributeName");
-				
-		IData attributeNameIData = attributeNameList.get(0);
-		
+		// Dereference these lists to get at the actual value (not sure why we have to do this)
 		FeatureCollection obsFc = ((GTVectorDataBinding)obsList.get(0)).getPayload();
 		double maxRange = ((LiteralDoubleBinding)maxList.get(0)).getPayload();
 		double minRange = ((LiteralDoubleBinding)minList.get(0)).getPayload();
 		String nameString = ((LiteralStringBinding)attributeNameList.get(0)).getPayload();
 		
-		SimpleFeatureIterator sfi = (SimpleFeatureIterator) obsFc.features();
-		SimpleFeature tempPropFeature = sfi.next();
+		// Get the coordinate system of the input observations
 		CoordinateReferenceSystem inputObsCrs = obsFc.getSchema().getCoordinateReferenceSystem();
 		
+		// Use the first feature from the feature collection as template for output...
+		SimpleFeatureIterator sfi = (SimpleFeatureIterator) obsFc.features();
+		SimpleFeature tempPropFeature = null;		// temporary feature from which to extract properties
+		
+		try {		
+			tempPropFeature = sfi.next();
+		} finally {
+			// ensure we release resources to the OS
+			sfi.close();			
+		}
+		
+		// Get the properties of the first feature to act as template
 		Collection<Property> obsProp = tempPropFeature.getProperties();
-		
-		
-		
-		//SimpleFeatureType typeF = tempPropFeature.getType();
-		
+	
+		// Set up the type builder for the results
 		SimpleFeatureTypeBuilder resultTypeBuilder = new SimpleFeatureTypeBuilder();
 		resultTypeBuilder.setName("typeBuilder");
 		resultTypeBuilder.setCRS(inputObsCrs);
 		
+		// Iterate through the properties, adding them to the type builder
 		Iterator<Property> pItObs = obsProp.iterator();
-		
-	
-		
-		sfi.close();
-		while (pItObs.hasNext()==true){
+		while (pItObs.hasNext()){
+			try {
+				Property tempProp = pItObs.next();
 			
-			try{
+				PropertyType type = tempProp.getType();
+				String name = type.getName().getLocalPart();
+				Class<?> valueClass = (Class<?>)tempProp.getType().getBinding();
 				
-			Property tempProp = pItObs.next();
-		
-			PropertyType type = tempProp.getType();
-			String name = type.getName().getLocalPart();
-			Class<?> valueClass = (Class<?>)tempProp.getType().getBinding();
-			
-			resultTypeBuilder.add(name, valueClass);
-			
-
-			LOG.warn ("Obs property " + " name " +  name + " class<?> " + valueClass +
-					" type " + type + " tempProp.getValue() " + tempProp.getValue() );
+				resultTypeBuilder.add(name, valueClass);
+				
+				LOG.warn ("Obs property " + " name " +  name + " class<?> " + valueClass +
+						" type " + type + " tempProp.getValue() " + tempProp.getValue() );
 			}
 			catch (Exception e){
 				LOG.error("property error " + e);
 			}
 		}
 		
-		//add DQ_Field
-		
+		// add DQ_Field (to store success status)
 		resultTypeBuilder.add("DQ_ValueRange", Double.class);
-		
+		// Build the FeatureType
 		SimpleFeatureType typeF = resultTypeBuilder.buildFeatureType();
-		LOG.warn("Get Spatial Accuracy Feature Type " + typeF.toString());
 		
 		LOG.warn("++++++++++++++ HERE +++++++++++++");
 		LOG.warn("obsFc " + obsFc.size());
 		LOG.warn("maxRange " + maxRange);
 		LOG.warn("minRange " + minRange);
 		LOG.warn("nameString " + nameString);
-		SimpleFeatureBuilder resultFeatureBuilder = new SimpleFeatureBuilder(typeF);
 		
-				
+		// Make a result feature builder from the FeatureType
+		SimpleFeatureBuilder resultFeatureBuilder = new SimpleFeatureBuilder(typeF);
+		// Lists for the two result lists
 		ArrayList<SimpleFeature> resultArrayList = new ArrayList<SimpleFeature>(); 
-		ArrayList<SimpleFeature> qual_resultArrayList = new ArrayList<SimpleFeature>();
-		SimpleFeatureIterator obsIt = (SimpleFeatureIterator) obsFc.features();
+		ArrayList<SimpleFeature> qual_resultArrayList = new ArrayList<SimpleFeature>();	
 		
 		LOG.warn("Attribute Range Feature Type " + typeF.toString() );
-		obsIt.close();
-		SimpleFeatureIterator obsIt2 = (SimpleFeatureIterator) obsFc.features();
 		
-		int within = 0;
-		while (obsIt2.hasNext()==true){
-			
-			SimpleFeature tempSf = obsIt2.next();	
-			within = 0;
-			for (Property obsProperty : tempSf.getProperties()){
-
+		// Now go through the observations and actually do the check
+		SimpleFeatureIterator obsIt2 = (SimpleFeatureIterator) obsFc.features();
+		try {
+			while (obsIt2.hasNext()) {
+				boolean within;
+				SimpleFeature tempSf = obsIt2.next();
 				
-				String name = obsProperty.getName().toString();
-				Object value = obsProperty.getValue();
-				
-				resultFeatureBuilder.set(name, value);
-				
-			}
-			//Property tempAttribute =  tempSf.getProperty(nameString);
-			
-		//	LOG.warn("tempFeature " + tempSf.toString() );
-			
-			//LOG.warn("doubleValue " + tempAttribute.getValue().toString());
-			
-			double tempAttributeValue = Double.parseDouble(tempSf.getProperty(nameString).getValue().toString());
-			resultFeatureBuilder.set("DQ_ValueRange", 0);
-			if (tempAttributeValue>= minRange){
-				
-				if(tempAttributeValue<=maxRange){
-					within = 1;
-					resultFeatureBuilder.set("DQ_ValueRange", 1);
+				// Copy feature properties to include in output
+				for (Property obsProperty : tempSf.getProperties()){	
+					String name = obsProperty.getName().toString();
+					Object value = obsProperty.getValue();
+					resultFeatureBuilder.set(name, value);
 				}
 				
-		
+				// Now calculate and set the DQ value (do the check!)
+				double tempAttributeValue = Double.parseDouble(tempSf.getProperty(nameString).getValue().toString());
+				if(ValRange.valueInRange(tempAttributeValue, minRange, maxRange)) {
+					within = true;
+					resultFeatureBuilder.set("DQ_ValueRange", 1);
+				} else {
+					within = false;
+					resultFeatureBuilder.set("DQ_ValueRange", 0);
+				}
+				
+				// Build the completed feature and set its geometry from the input feature
+				SimpleFeature tempResult = resultFeatureBuilder.buildFeature(tempSf.getID());
+				Geometry tempGeom = (Geometry) tempSf.getDefaultGeometry();
+				tempResult.setDefaultGeometry(tempGeom);
+				
+				// add the feature to qual_result if it passes the test
+				if(within) {
+					qual_resultArrayList.add(tempResult);
+				}
+				// add it it to the results with its DQ set anyway
+				resultArrayList.add(tempResult);
 			}
-			SimpleFeature tempResult = resultFeatureBuilder.buildFeature(tempSf.getID());
-			Geometry tempGeom = (Geometry) tempSf.getDefaultGeometry();
-			tempResult.setDefaultGeometry(tempGeom);
-			resultArrayList.add(tempResult);
-			if(within == 1){
-				qual_resultArrayList.add(tempResult);
-			}
-			
+		} finally {
+			// safely release resources back to the OS
+			obsIt2.close();
 		}
-		obsIt2.close();
+		
+		// Make feature collections from the lists of features
 		FeatureCollection resultFeatureCollection = new ListFeatureCollection(typeF, resultArrayList);
 		FeatureCollection qual_resultFeatureCollection = new ListFeatureCollection(typeF, qual_resultArrayList);
 
-		
+		// Add them to the result hash map
 		HashMap<String, IData> results = new HashMap<String, IData>();
 		results.put("result", new GTVectorDataBinding((FeatureCollection)resultFeatureCollection));
 		results.put("qual_result", new GTVectorDataBinding((FeatureCollection)qual_resultFeatureCollection));
-		
-		
 		
 		return results;
 	}
@@ -231,9 +214,6 @@ public class ValueRange extends AbstractAlgorithm{
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	
-
 }
 
 
